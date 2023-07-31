bba1afed319978528e1f342a34148ca2f492a17a
Kirill_Markov <enifl33fi@gmail.com>
2023-07-31 08:10:02 +0300

    source for registration

---
+++ src/main/java/ru/spring/school/online/controller/LoginController.java
@@ -0,0 +1,21 @@
+package ru.spring.school.online.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.spring.school.online.model.security.RawUserForm;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+    @ModelAttribute(name = "userForm")
+    public RawUserForm form(){
+        return new RawUserForm();
+    }
+
+    @GetMapping
+    public String login(){
+        return "login";
+    }
+}
