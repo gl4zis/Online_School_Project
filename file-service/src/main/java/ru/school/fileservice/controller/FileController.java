9b84f3712a756c7e5180a02c7fbd02b1783d57f6
gl4zis <maker64@inbox.ru>
2023-12-07 01:34:13 +0300

    file service documentation

--- file-service/src/main/java/ru/school/fileservice/controller/FileController.java
+++ file-service/src/main/java/ru/school/fileservice/controller/FileController.java
@@ -1,5 +1,7 @@
 package ru.school.fileservice.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -13,23 +15,28 @@ import java.io.FileNotFoundException;
 @RestController
 @ResponseBody
 @RequiredArgsConstructor
+@Tag(name = "User file controller")
 public class FileController {
     private final FileService fileService;
 
-    // 404 (FileNotFound), 400 (Invalid id)
+    @Operation(summary = "Get file", description = "Returns file base64 by id (long). " +
+            "Throws 404 (NotFound), 400 (InvalidId)")
     @GetMapping("/{id}")
     public String getFile(@PathVariable("id") Long id) throws FileNotFoundException {
         return fileService.getFileBase64(id);
     }
 
-    // 400 (InvalidFile)
+    @Operation(summary = "Save new file", description = "Creates new file in DB. " +
+            "If client has valid access token, file will has this client as an owner (can be null). " +
+            "Throws 400 (InvalidFile)")
     @PostMapping
     public Long createFile(@RequestBody MultipartFile file, HttpServletRequest request)
             throws InvalidFileException {
         return fileService.saveNewFile(file, request);
     }
 
-    // 403 (NoAccess | InvalidToken)
+    @Operation(summary = "Removes file from DB", description = "Access only for admins or owner. " +
+            "Throws 403 (NoAccess, InvalidToken)")
     @DeleteMapping("/{id}")
     public void removeFile(@PathVariable("id") Long id, HttpServletRequest request)
             throws InvalidTokenException {
