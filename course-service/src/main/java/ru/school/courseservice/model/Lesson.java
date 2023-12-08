72d590687f6c963c01eef6d05096e759b9541827
gl4zis <maker64@inbox.ru>
2023-12-08 15:20:25 +0300

    get all courses endpoint

---
+++ course-service/src/main/java/ru/school/courseservice/model/Lesson.java
@@ -0,0 +1,22 @@
+package ru.school.courseservice.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.Date;
+
+@Entity
+@Data
+public class Lesson {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne(optional = false)
+    private Group group;
+    @Column(nullable = false)
+    private Date planedAt;
+    @Column(nullable = false)
+    private String title;
+    private Long attachmentFileId;
+    private String meetingUrl;
+}
